84c7ef251200d3e4305e260aaa49c5b90844130d
Tony Stone <apstone45@gmail.com>
2025-10-22 09:09:20 -0400

    Add files via upload

--- lessonVI.py
+++ lessonVI.py
@@ -22,8 +22,43 @@ def exampleThree():
     print(fileContent.split('\n')[-1])
     file.close()
 
+def exampleFour():
+    print("\nExample Four:")
+    file = open('basicFolder/afile.txt', 'w')
+    file.write("Hello!")
+    file.close
+
+def exampleFive():
+    print("\nExample Five:")
+    file = open('basicFolder/afile.txt', 'a')
+    file.write("Hello agian!")
+    file.close
+
+def exampleSix():
+    print("\nExample Six:")
+    file = open('basicFolder/afile.txt', 'w')
+    file.write("Hello!")
+    file.close
+
+    file = open('basicFolder/afile.txt', 'a')
+    file.write(" Hello agian!")
+    file.close
+
+def exampleSeven():
+    print("\nExample Seven:")
+    content = ["Hello! \n",
+               "Hello again! \n"
+               "Sh-boom"]
+    file = open('basicFolder/afile.txt', 'w')
+    file.writelines(content)
+    file.close
+
 #==================================================================
 # Comment and uncomment as you please to explore specific examples!
-exampleOne()
-exampleTwo()
-exampleThree()
+#exampleOne()
+#exampleTwo()
+#exampleThree()
+#exampleFour()
+#exampleFive()
+#exampleSix()
+exampleSeven()
